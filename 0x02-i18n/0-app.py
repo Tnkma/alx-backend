ef72eaf5ba1ea477b5b96b3beee2357bc294aba4
Tnkma <onwusilikegodswill@gmail.com>
2024-05-28 13:17:18 +0100

    :test_tube: initializing the flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,11 +1,18 @@
+#!/usr/bin/env python3
+"""Flask app that renders an index page"""
 from flask import Flask, render_template
+
 
 # Flask app initialization
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():
+def index() -> str:
     """render the index page
     """
-    return render_template('index.html')
+    return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
