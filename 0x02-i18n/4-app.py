82cc7045cce7acda29834c8f20d43e6393b0490b
Tnkma <onwusilikegodswill@gmail.com>
2024-05-30 02:39:10 +0100

    getting get_locale method to return URL parameter

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Flask app that renders an index page"""
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 from flask_babel import Babel
 
 
@@ -22,10 +22,10 @@ babel = Babel(app)
 def index() -> str:
     """render the index page
     """
-    return render_template('3-index.html')
+    return render_template('4-index.html')
 
 
-@babel.localeselector
+# @babel.localeselector
 def get_locale() -> str:
     """gets the best matching language for the user
 
@@ -34,15 +34,24 @@ def get_locale() -> str:
         We can say 'locale = request.args.get('locale')' to get the locale
         but its also good i learned to use lambda function to get the locale
     """
-    queries = request.query_string.decode('utf-8').split('&')
-    query_table = dict(map(
-        lambda x: (x if '=' in x else '{}='.format(x)).split('='),
-        queries,
-    ))
-    if 'locale' in query_table:
-        if query_table['locale'] in app.config["LANGUAGES"]:
-            return query_table['locale']
+    # queries = request.query_string.decode('utf-8').split('&')
+    # query_table = dict(map(
+        # lambda x: (x if '=' in x else '{}='.format(x)).split('='),
+        # queries,
+    # ))
+    # if 'locale' in query_table:
+        # if query_table['locale'] in app.config["LANGUAGES"]:
+            # return query_table['locale']
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        session[locale] = locale
+        return locale
+    if 'locale' in session:
+        return session['locale']
     return request.accept_Languages.best_match(app.config['LANGUAGES'])
+
+
+babel.init_app(app, get_locale)
 
 
 if __name__ == "__main__":
